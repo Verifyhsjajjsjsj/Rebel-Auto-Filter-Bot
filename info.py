cffce3977fa571b67f4ac8a6d83156689a0eb829
Verifyhsjajjsjsj <mykakjsjjsos@gmail.com>
2025-01-27 09:20:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
         return default
 
 #main variables
-API_ID = int(environ.get('API_ID', ''))
+API_ID = int(environ.get('API_ID', '26162072'))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
